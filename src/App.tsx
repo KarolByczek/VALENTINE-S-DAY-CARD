b416bdc0f82ae6e21507cb716fd0565692a57b00
Karol Byczek <byczek_1986@interia.pl>
2024-01-12 08:58:40 +0100

    Utilization of other images for the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Pictureswitcher } from "./Pictureswitcher";
 import Pictureone from "./images/IMG_20230401_222604.jpg";
+import Picturetwo from "./images/z0001062151q21.jpg";
+import Picturethree from "./images/z0001062152vi2.jpg";
 import "./App.scss";
 
 function App() {
@@ -10,6 +12,16 @@ function App() {
         className={"image"}
         source={Pictureone}
         alternate={"photo one"}
+      />
+      <Pictureswitcher
+        className={"image"}
+        source={Picturetwo}
+        alternate={"photo two"}
+      />
+      <Pictureswitcher
+        className={"image"}
+        source={Picturethree}
+        alternate={"photo three"}
       />
     </div>
   );
