313ac1a0f7d9c7b69f7fb31e51d58744c2d18b44
Karol Byczek <byczek_1986@interia.pl>
2024-02-13 23:49:49 +0100

    Finishing touches.

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,25 @@ import "./App.scss";
 
 const Pictureswitcher = (event:MouseEvent<HTMLImageElement>) => {
   if (event.currentTarget.classList.contains('Kasia_Kozak')) {
-    const $images:NodeListOf<Element> = document.querySelectorAll('.image');
-    $images.forEach((image:Element) => {
+    const $hearts:NodeListOf<Element> = document.querySelectorAll('.heart');
+    $hearts.forEach((image:Element) => {
       image.classList.add('active');
-      image.classList.remove('image')
+      image.classList.remove('heart')
     });
   }
    else {
     const $images:NodeListOf<Element> = document.querySelectorAll('.active');
     $images.forEach((image:Element) => {
       image.classList.remove('active');
-      image.classList.add('image')
+      image.classList.add('heart')
     });
   }
 };
 
-const ListBrightener = (event:MouseEvent<HTMLUListElement>) => {
-  event.currentTarget.style.color = "green";
-};
-
-const ListDarkener = (event:MouseEvent<HTMLUListElement>) => {
-  event.currentTarget.style.color = "black";
-};
 
 
 function App() {
   
-  const [store, setStore] = useState<string>("");
-
-  const useInputHandler = (event:ChangeEvent<HTMLInputElement>) => {
-   setStore(event.currentTarget.value) 
-  }; 
-
 
   return (
     <div className="App">
