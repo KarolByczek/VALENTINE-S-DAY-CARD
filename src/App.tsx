e25d7b1abb614f773dc91271aae5547a2931054b
Karol Byczek <byczek_1986@interia.pl>
2024-02-13 23:16:28 +0100

    Removal of all the unnecessary components in preparation for the general overhaul of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 
 import { useState, ChangeEvent, MouseEvent } from "react";
 import { Picture } from "./components/Pictureswitcher";
-import { ExampleForm } from "./components/ExampleForm";
-import { ExampleList } from "./components/ExampleList";
-import Pictureone from "./images/IMG_20230401_222604.jpg";
-import Picturetwo from "./images/z0001062151q21.jpg";
-import Picturethree from "./images/z0001062152vi2.jpg";
+
 import "./App.scss";
 
 
@@ -46,33 +42,9 @@ function App() {
 
   return (
     <div className="App">
-      <div className="images">
       <Picture
-        className={"image"}
-        source={Pictureone}
-        alternate={"photo01"}
-        onClick={Pictureswitcher}
+      
       />
-      <Picture
-        className={"image"}
-        source={Picturetwo}
-        alternate={"photo02"}
-        onClick={Pictureswitcher}
-      />
-      <Picture
-        className={"image"}
-        source={Picturethree}
-        alternate={"photo03"}
-        onClick={Pictureswitcher}
-      />
-      </div>
-      <div className="inputs">
-        <ExampleForm type={"text"} onChange={useInputHandler} value={store}/>
-        <ExampleForm type={"text"} onChange={() => {}} value={store} />
-      </div>
-      <div className="list">
-        <ExampleList children={store} onHover={ListBrightener} onRemove={ListDarkener}/>
-      </div>
     </div>
   );
 }
