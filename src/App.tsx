61ef000a32ac09bff056f329da6414245ecc9574
Karol Byczek <byczek_1986@interia.pl>
2024-02-13 23:26:59 +0100

    Some initial preparatory actions...

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 
 import { useState, ChangeEvent, MouseEvent } from "react";
 import { Picture } from "./components/Pictureswitcher";
-
+import Kasia from './images/10.jpeg';
+import Heart from './images/Red-Heart-Love.png.png';
 import "./App.scss";
 
 
@@ -43,7 +44,10 @@ function App() {
   return (
     <div className="App">
       <Picture
-      
+        className="Kasia_Kozak"
+        source={Kasia}
+        alternate="Kasia"
+        onClick={() => window.alert('Kliknij mnie! :)')}
       />
     </div>
   );
