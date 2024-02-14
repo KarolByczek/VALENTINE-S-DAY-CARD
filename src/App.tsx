048305b492045fb1a07599bd6d556494fe8b76f4
Karol Byczek <byczek_1986@interia.pl>
2024-02-14 14:14:14 +0100

    Initiation of responsive design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState, ChangeEvent, MouseEvent } from "react";
+import { MouseEvent } from "react";
 import { Picture } from "./components/Pictureswitcher";
 import Kasia from './images/10.jpeg';
 import Heart from './images/Red-Heart-Love.png.png';
@@ -9,17 +9,18 @@ import "./App.scss";
 const Pictureswitcher = (event:MouseEvent<HTMLImageElement>) => {
   if (event.currentTarget.classList.contains('Kasia_Kozak')) {
     const $hearts:NodeListOf<Element> = document.querySelectorAll('.heart');
+    const $clue: any = document.querySelector('#clue');
+    const $signs: any = document.querySelectorAll('.sign');
     $hearts.forEach((image:Element) => {
       image.classList.add('active');
-      image.classList.remove('heart')
+      image.classList.remove('heart');
+      
     });
-  }
-   else {
-    const $images:NodeListOf<Element> = document.querySelectorAll('.active');
-    $images.forEach((image:Element) => {
-      image.classList.remove('active');
-      image.classList.add('heart')
+    $signs.forEach((sign:HTMLElement) => {
+      sign.classList.add('sign_vis');
     });
+    $clue.style.display = 'none'
+    //window.alert('WSZYSTKIEGO NAJLEPSZEGO Z OKAZJI WALENTYNEK!!!:)')
   }
 };
 
@@ -36,12 +37,92 @@ function App() {
         alternate="Kasia"
         onClick={Pictureswitcher}
       />
-      <h3>KLIKNIJ KASIĘ ⬆</h3>
+      <h3 id="clue">KLIKNIJ KASIĘ ⬆</h3>
+      <h1 className="sign sign_left">WSZYSTKIEGO</h1>
+      <h1 className="sign sign_right">NAJLEPSZEGO!!!</h1>
       <Picture 
         className="heart heart01"
         source={Heart}
         alternate="heart"
-        onClick={() => window.alert('WSZYSTKIEGO NAJLEPSZEGO Z OKAZJI WALENTYNEK!!!:)')}
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart02"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart03"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart04"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart05"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart06"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart07"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart08"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart09"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart10"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart11"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart12"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart13"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
+      />
+      <Picture 
+        className="heart heart14"
+        source={Heart}
+        alternate="heart"
+        onClick={() => console.log('See ya later!')}
       />
     </div>
   );
